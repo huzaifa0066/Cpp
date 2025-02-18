5e7533f2228351f0cf7e1516809ae04a9ba5133b
Huzaifa Ali <mhuzaifa0066@gmail.com>
2025-02-18 22:48:14 +0500

    All Mathematicaal Operation

---
+++ Simple/Operations.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Input First Number:";
+	cin>>a;
+	cout<<"\nInput Second Number:";
+	cin>>b;
+	cout<<"\nAddition = "<<a+b;
+	cout<<"\nSubtraction = "<<a-b;
+	cout<<"\nMultiplication = "<<a*b;
+	cout<<"\nDivision = "<<a/b;
+	cout<<"\nRemainder = "<<a%b;
+	cout<<"\nAverage = "<<(a+b)/2.0;
+}
