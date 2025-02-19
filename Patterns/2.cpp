82fa580883519550ebee82c03e8d644744d743ec
Huzaifa Ali <mhuzaifa0066@gmail.com>
2025-02-19 21:40:40 +0500

    Patterns For loop

---
+++ Patterns/2.cpp
@@ -0,0 +1,15 @@
+// 1       2       3       4       5
+// 6       7       8       9       1
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int i,k=5,j=1;
+	for(i=1;i<=2;i++){
+		for(;j<=k;j++)
+		    cout<<j<<"\t";
+		k=10;
+		cout<<endl;
+	}	    	
+}
